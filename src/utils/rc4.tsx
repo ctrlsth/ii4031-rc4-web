feb84ee6cfb12319e03360f10da690202c8fa305
18221171 <18221171@std.stei.itb.ac.id>
2024-03-13 21:17:46 +0700

    feat: added affine modification in KSA

--- src/utils/rc4.tsx
+++ src/utils/rc4.tsx
@@ -1,10 +1,13 @@
-const BASE_ORD = 65;
 const MAX_LENGTH = 256;
 
-export function rc4(key: string, plaintext: string): String {
-  var ciphertext = new String();
+const relativelyPrime = (m: number): boolean => {
+  if (m % 2 == 0 || m % MAX_LENGTH == 0 || m <= 1) {
+    return false;
+  }
+  return true;
+};
 
-  /* Key-Scheduling Algorithm */
+function ksa(key: string, m: number, b:number): Array<number> {
   // 1. Buat Larik
   var S = new Array<number>(MAX_LENGTH);
   for (let x = 0; x < 256; x++) {
@@ -13,23 +16,41 @@ export function rc4(key: string, plaintext: string): String {
 
   // 2. Permutasikan (Acak) dengan Key
   var y = 0;
-  for (let x = 0; x < 256; x++) {
-    y = y + S[x] + (key.charCodeAt(x % key.length) % MAX_LENGTH);
+  for (let x = 0; x < MAX_LENGTH; x++) {
+    y = (y + S[x] + key.charCodeAt(x % key.length)) % MAX_LENGTH;
     [S[x], S[y]] = [S[y], S[x]];
   }
+
+  // MODIFICATION
+  // 3. Ubah Value Stream dengan Konsep Affine Cipher
+  if (relativelyPrime(m)) {
+    for (let x = 0; x < MAX_LENGTH; x++) {
+      S[x] = (m * S[x] + b) % MAX_LENGTH;
+    }
+  }
+
+  return S;
+}
+
+export function rc4(key: string, plaintext: string, m: number, b: number): String {
+  var ciphertext = new String("");
+
+  /* Key-Scheduling Algorithm */
+  var S = ksa(key, m, b);
 
   /* Pseudo-Random Generation Algorithm */
   // 1. Ambil S[i] dan S[j]
   // 2. Tukar Value
   // 3. Jumlahkan S[i] dan S[j], lalu modulasikan dengan MAX_LENGTH
-  // 4. Gunakan hasil pada langkah ke-3 sebagai indeks dari KUNCI ALIR yang terpilih
-  // 5. Enkripsi plainteks dengan KUNCI ALIR
   var i = 0;
   var j = 0;
   for (let x = 0; x < plaintext.length; x++) {
     i = (i + 1) % MAX_LENGTH;
     j = (j + S[i]) % MAX_LENGTH;
     [S[i], S[j]] = [S[j], S[i]];
+    // BISA DI ALTER PAS SWAP JG
+
+    /* Enkripsikan */
     var c = S[(S[i] + S[j]) % MAX_LENGTH] ^ plaintext.charCodeAt(x);
     ciphertext += String.fromCharCode(c);
   }
--- src/utils/vigenere.tsx
+++ src/utils/vigenere.tsx
@@ -9,9 +9,9 @@ export function autoKey(key: String, plaintext: String): String {
   if (key.length >= plaintext.length) {
     return key;
   } else {
-    return key += (plaintext.slice(0, plaintext.length - key.length));
+    return (key += plaintext.slice(0, plaintext.length - key.length));
   }
-};
+}
 
 export function loopKey(key: String, n: number): String {
   /* Ngulangin key ampe panjangnya sama kayak plaintext */
@@ -19,7 +19,7 @@ export function loopKey(key: String, n: number): String {
     return key;
   } else {
     var x = 0;
-    var fk = new String(key)
+    var fk = new String(key);
     while (fk.length != n) {
       fk += key.charAt(x);
       x++;
@@ -29,23 +29,27 @@ export function loopKey(key: String, n: number): String {
     }
     return fk;
   }
-};
+}
 
-export function encVigenere(plaintext: String, key: String, type: number): String {
+export function encVigenere(
+  plaintext: String,
+  key: String,
+  type: number
+): String {
   var fullkey = new String("");
   var ciphertext = new String("");
 
   if (type == 2) {
-    key = sanitizeKey(key)
+    // key = sanitizeKey(key);
     fullkey = loopKey(key, plaintext.length);
     for (let x = 0; x < plaintext.length; x++) {
-      var cipherNum = ((plaintext.charCodeAt(x) - BASE_ORD + (fullkey.charCodeAt(x) - BASE_ORD)) % 256) + BASE_ORD;
+      var cipherNum = (plaintext.charCodeAt(x) + fullkey.charCodeAt(x)) % 256;
       ciphertext += String.fromCharCode(cipherNum); //String.fromCharCode ngubah ASCII ke Character
     }
   } else {
     plaintext = sanitizeKey(plaintext);
     key = sanitizeKey(key);
-  
+
     if (type == 0) {
       fullkey = loopKey(key, plaintext.length);
     } else {
@@ -54,27 +58,35 @@ export function encVigenere(plaintext: String, key: String, type: number): Strin
 
     for (let x = 0; x < plaintext.length; x++) {
       /* cipherNum adalah huruf dalam plaintext yang diconvert ke ASCII kemudian dienkripsi memakai key */
-      const cipherNum = ((plaintext.charCodeAt(x) - BASE_ORD + (fullkey.charCodeAt(x) - BASE_ORD)) % 26) + BASE_ORD;
+      const cipherNum =
+        ((plaintext.charCodeAt(x) -
+          BASE_ORD +
+          (fullkey.charCodeAt(x) - BASE_ORD)) %
+          26) +
+        BASE_ORD;
       ciphertext += String.fromCharCode(cipherNum); //String.fromCharCode ngubah ASCII ke Character
     }
   }
   return ciphertext;
 }
 
-export function decVigenere(ciphertext: String, key: String, type: number): String {
+export function decVigenere(
+  ciphertext: String,
+  key: String,
+  type: number
+): String {
   var fullkey = new String("");
   var decryptedtext = new String("");
 
   if (type == 2) {
-    key = sanitizeKey(key)
     fullkey = loopKey(key, ciphertext.length);
 
     for (let x = 0; x < ciphertext.length; x++) {
-      var cipherNum = ((ciphertext.charCodeAt(x) - BASE_ORD - (fullkey.charCodeAt(x) - BASE_ORD)) % 256) + BASE_ORD;
+      var cipherNum = (ciphertext.charCodeAt(x) - fullkey.charCodeAt(x)) % 256;
       if (cipherNum < 0) {
         cipherNum += 256;
       }
-      decryptedtext += (String.fromCharCode(cipherNum));
+      decryptedtext += String.fromCharCode(cipherNum);
     }
   } else {
     ciphertext = sanitizeKey(ciphertext);
@@ -87,12 +99,17 @@ export function decVigenere(ciphertext: String, key: String, type: number): Stri
     }
 
     for (let x = 0; x < ciphertext.length; x++) {
-      var cipherNum = ((ciphertext.charCodeAt(x) - BASE_ORD - (fullkey.charCodeAt(x) - BASE_ORD)) % 26) + BASE_ORD;
+      var cipherNum =
+        ((ciphertext.charCodeAt(x) -
+          BASE_ORD -
+          (fullkey.charCodeAt(x) - BASE_ORD)) %
+          26) +
+        BASE_ORD;
       if (cipherNum < 65) {
         cipherNum += 26;
       }
-      (type == 1) ? fullkey += String.fromCharCode(cipherNum) : null;
-      decryptedtext += (String.fromCharCode(cipherNum));
+      type == 1 ? (fullkey += String.fromCharCode(cipherNum)) : null;
+      decryptedtext += String.fromCharCode(cipherNum);
     }
   }
   return decryptedtext;
